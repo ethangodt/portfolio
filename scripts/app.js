04416ae46b7e3f43d73a08c465af5c27983843db
Ethan Godt <ethan@ethangodt.com>
2020-05-09 17:40:20 -0700

    better paypal images

--- scripts/app.js
+++ scripts/app.js
@@ -20,15 +20,15 @@ window.router = new Router(
     fallback: "/about",
     "/about": getRoute("about", "#2d2d2d", [new Card(about, "#b5b5b5")]),
     "/paypal": getRoute("paypal", "#0070ba", [
-      new Card(p2p, "#dcd9f5"),
-      new Card(gifting, "#bb8be6"),
-      new Card(jaws, "red"),
+      new Card(p2p, "#8fc4fb"),
+      new Card(gifting, "#3b9a47"),
+      new Card(jaws, "#bb8be6"),
     ]),
-    "/misc": getRoute("side projects", "#7a88d0", [new Card(songlink, "red")]),
-    "/design": getRoute("design", "#7a88d0", [
-      new Card(plantMW, "#4232c7"),
-      new Card(crossfit, "#b77272"),
-      new Card(ekk, "red"),
+    "/misc": getRoute("side projects", "#5a5d6d", [new Card(songlink, "#d03608")]),
+    "/design": getRoute("design", "#987676", [
+      new Card(plantMW, "#400000"),
+      new Card(crossfit, "red"),
+      new Card(ekk, "black"),
     ]),
   },
   APP_CONTAINER
